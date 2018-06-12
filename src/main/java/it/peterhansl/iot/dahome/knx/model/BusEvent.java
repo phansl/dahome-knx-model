4323e691afc0fcd273dd6538b5fc58040bf1013d
phansl <philip@peterhansl.de>
2018-06-12 21:14:22 +0200

    Upgraded to Spring Boot 2.0.2.RELEASE and implemented lombok on BusEvent class.

--- src/main/java/it/peterhansl/iot/dahome/knx/model/BusEvent.java
+++ src/main/java/it/peterhansl/iot/dahome/knx/model/BusEvent.java
@@ -3,22 +3,49 @@ package it.peterhansl.iot.dahome.knx.model;
 import java.time.Instant;
 import java.util.UUID;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import tuwien.auto.calimero.dptxlator.DPTXlator;
+import tuwien.auto.calimero.dptxlator.TranslatorTypes;
+import tuwien.auto.calimero.exception.KNXException;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class BusEvent {
 
-	private final String id;
-	private final long timestamp;
-	private final String name;
-	private final int address;
-	private final String dpt;
-	private final byte[] value;
+	private  String id;
+	private  long timestamp;
+	private  String name;
+	private  int address;
+	private  int mainNumber;
+	private  String dpt;
+	private  byte[] rawValue;
 	
-	public BusEvent(String name, int address, String dpt, byte[] value) {
-		this(UUID.randomUUID().toString(), Instant.now().toEpochMilli(), name, address, dpt, value);
+	//@Setter(AccessLevel.PRIVATE)
+	private  double numericValue;
+	
+	//@Setter(AccessLevel.PRIVATE)
+	private  String value;
+	
+	public BusEvent(String name, int address, int mainNumber, String dpt, byte[] rawValue) throws IllegalArgumentException {
+		this(UUID.randomUUID().toString(), Instant.now().toEpochMilli(), name, address, mainNumber, dpt, rawValue, 0.0d, "");
+		try {
+			calcAndSetValue();
+		} catch (KNXException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	private void calcAndSetValue() throws KNXException {
+		DPTXlator xlator = TranslatorTypes.createTranslator(getMainNumber(), getDpt());
+		xlator.setData(getRawValue());
+		this.setNumericValue(xlator.getNumericValue());
+		this.setValue(xlator.getValue());
 	}
 	
 }
